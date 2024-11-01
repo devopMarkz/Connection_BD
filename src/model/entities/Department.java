26e0978a1829888d0084d1519d743540b1fca577
devopMarkz <marcosdev2002@gmail.com>
2024-11-01 19:28:38 -0300

    Comentando o código no estilo JavaDoc

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,32 +1,61 @@
 package model.entities;
 
+/**
+ * Classe que representa um departamento com informações de identificação e nome.
+ */
 public class Department {
 
     private Long id;
     private String name;
 
-    public Department(Long id, String name){
+    /**
+     * Construtor para criar uma instância de Department com o ID e nome especificados.
+     *
+     * @param id   o ID do departamento.
+     * @param name o nome do departamento.
+     */
+    public Department(Long id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    /**
+     * Retorna o ID do departamento.
+     *
+     * @return o ID do departamento.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Retorna o nome do departamento.
+     *
+     * @return o nome do departamento.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Define o nome do departamento.
+     *
+     * @param name o novo nome do departamento.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Retorna uma representação em String do objeto Department.
+     *
+     * @return uma String com as informações do departamento.
+     */
     @Override
     public String toString() {
         return "Department{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
                 '}';
     }
 }
